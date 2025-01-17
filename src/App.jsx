b5495abada6b4c07787ca32abd1171cd87468875
kaydepage <kaydepage@outlook.com>
2025-01-17 11:36:25 +1100

    Added header, searchBar and search components

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,45 @@ import {
   BrowserRouter as Router,
   Routes, Route, Link
 } from 'react-router-dom'
+import { useState } from 'react';
 
+const Header = () => {
+  return (
+    <div>
+
+      <SearchBar />
+      <Link to="/">home</Link>
+      <Link to="/advanced">advanced</Link>
+    </div>
+  )
+}
+
+const SearchBar = () => {
+  return (
+    <div>
+      <form action="/search">
+        <input type="text"/>
+      </form>
+    </div>    
+  )
+}
 
 const Home = () => {
 
   return (
     <div>
+
       <h1>
         Enter something in the textbox to search, or don't
       </h1>
-      <form>
-        <input type="text"/>
-      </form>
+
+      <SearchBar />
+        
+      <div>
+        <Link to="/">home</Link>
+        <Link to="/advanced">advanced</Link>
+      </div>
+
     </div>
   )
 }
@@ -22,9 +49,32 @@ const Advanced = () => {
 
   return (
     <div>
+      <Header />
       <h1>
         Advanced search
       </h1>
+
+    </div>
+  )
+}
+
+const Search = (props) => {
+
+  const { cards } = props
+  if (cards.length === 0) {
+    return (
+      <div>
+        No cards returned in search. Try searching something else.
+      </div>
+    )
+  }
+  return (
+    
+    <div>
+      <Header />
+      <div>
+        {cards.map(card => <div>{card.content}</div>)}
+      </div>
     </div>
   )
 }
@@ -32,25 +82,18 @@ const Advanced = () => {
 
 const App = () => {
 
-  const padding = {
-    padding: 5
-  }
+  const [cards, setCards] = useState([1])
 
   return (
     <Router>
-      <div>
-        <Link style={padding} to="/">home</Link>
-        <Link style={padding} to="/advanced">advanced</Link>
-      </div>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/advanced" element={<Advanced />} />
-        <Route path="/search" element={<Search />} />
+        <Route path="/search" element={<Search cards={cards}/>} />
       </Routes>
 
       <div>
-        <i>Note app, Department of Computer Science 2024</i>
+        <i>Footer</i>
       </div>
     </Router>
   )
